0eb0ecd567b970be005f956528b146a0a484811d
Sairainc <sairatechcompany@gmail.com>
2025-02-23 16:08:23 +0900

    feat: 料金プランを更新（男性4,980円、女性2,980円）

--- src/components/SafetyFeatures.tsx
+++ src/components/SafetyFeatures.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ShieldCheckIcon, UserGroupIcon, LockClosedIcon, CheckBadgeIcon } from '@heroicons/react/24/solid'
+import { ShieldCheckIcon } from '@heroicons/react/24/solid'
 
 interface SafetyFeatureProps {
   title: string
