6c528ec3e566d7e5a1da687d0a160fcedbff7442
Sairainc <sairatechcompany@gmail.com>
2025-03-23 23:54:29 +0900

    matchpageのDB確認

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -149,9 +149,6 @@ export default function MatchConfirmation() {
     const year = date.getFullYear()
     const month = date.getMonth() + 1
     const day = date.getDate()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-
     
     return `${year}年${month}月${day}日 19時〜`
   }
